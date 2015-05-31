cd9b191f7abc29389dd99e92cbfb7d5e30b35949
mlmedia <mike@millionlightsmedia.com>
2015-05-30 23:46:32 -0400

    - adding JS / CSS + basic markup

--- js/tabs.js
+++ js/tabs.js
@@ -5,46 +5,48 @@
 ( function( $ )
 {
 	/* tabs - my custom plugin */
-	$.fn.tabs = function( ) 
+	$.fn.tabs = function( )
 	{
 		/* set vars */
 		var tab_group 		= this;
+		console.log(tab_group);
 		var tab_controls 	= tab_group.find( '.tab_control' );
 		var active_tabs 	= tab_group.find( '.tab_control.current' );
 		var tab_targets 	= tab_group.find( '.tab_target' );
 		var active_tab 		= active_tabs.first( ).length > 0 ? active_tabs.first( ) : tab_group.find( '.tab_control:eq(0)' );
 		var target_id		= active_tab.attr( 'rel' );
 		var target_el		= tab_group.find( '.tab_target#' + target_id );
-		
+
 		/* set the css */
 		tab_group.hide( );
-		$( window ).load( function( ) {
-			
+		$( window ).load( function( )
+		{
 			/* grab the active tab and hide all other tabs */
 			tab_controls.removeClass( 'current' );
 			active_tab.addClass( 'current' );
 			tab_targets.hide( );
 			target_el.show( );
-			
+
 			/* show the tabs when everything is loaded */
 			tab_group.show( );
-			
 		});
-		
+
 		/* tab click */
 		tab_group.on( 'click', '.tab_control', function( e ) {
-			
 			/* set active class on the clicked tab */
 			tab_controls.removeClass( 'current' );
 			$( this ).addClass( 'current' );
-			
+
+			console.log('click');
+
 			/* grab the target tab and show it */
 			var target_id 	= $( this ).attr( 'rel' );
 			var target_el	= tab_group.find( '.tab_target#' + target_id );
+			console.log(target_el);
 			tab_targets.hide( );
 			target_el.show( );
 			e.preventDefault( );
 		});
 	}
-	
+
 })( jQuery );
